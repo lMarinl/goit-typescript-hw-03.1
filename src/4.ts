77b3b30d5b0d394ce9d363e3b002d7df76388c20
lMarinl <marutunan67@gmail.com>
2024-02-18 19:02:02 +0200

    ts 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,65 @@
-const key = new Key();
+class Key {
+  private signature: number
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  constructor() {
+    this.signature = Math.random()
+  }
 
-house.openDoor(person.getKey());
+  getSignature(): number {
+    return this.signature
+  }
+}
 
-house.comeIn(person);
+class Person {
+  private key: Key
 
+  constructor(key: Key) {
+    this.key = key
+  }
 
-export {};
+  getKey(): Key {
+    return this.key
+  }
+}
+
+abstract class House {
+  protected door: boolean
+  protected key: Key
+  protected tenants: Person[] = []
+
+  constructor(key: Key) {
+    this.key = key
+  }
+
+  abstract openDoor(key: Key): void
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person)
+      console.log("Person came in.")
+    } else {
+      console.log("The door is closed. Cannot come in.")
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true
+      console.log("Door opened.")
+    } else {
+      console.log("Invalid key. Door remains closed.")
+    }
+  }
+}
+
+const key = new Key()
+
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+
+house.comeIn(person)
+
+export {}
